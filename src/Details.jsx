0f422cb1b3ba4e06755379b766d23b117eb2537b
zain171m <zain171m@gmail.com>
2024-05-26 15:44:08 +0500

    Learned class components, Error boundaries, Refs and Portals in react

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,15 +1,20 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
+import Carousel from "./Carousel.jsx";
+import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
+import { useState } from "react";
 
 const Details = () => {
   const { id } = useParams();
   const result = useQuery(["details", id], fetchPet);
+  const [showModal, setShowModal] = useState(false);
 
   if (result.isLoading) {
     return (
       <div className="loading-pane">
-        <h2 className="loader">🔍</h2>
+        <h2 className="loader">🌀</h2>
       </div>
     );
   }
@@ -18,14 +23,32 @@ const Details = () => {
 
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
-        <button>Adopt Me!</button>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}!</button>
         <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <button>Yes</button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
 };
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
---
+++ src/Modal.jsx
@@ -0,0 +1,19 @@
+import { createPortal } from "react-dom";
+import React, { useEffect, useRef } from "react";
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal");
+    modalRoot.appendChild(elRef.current);
+    return () => modalRoot.removeChild(elRef.current);
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
